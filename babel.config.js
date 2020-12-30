7b9b342fdaecf4988416106f4174521775ce23f0
wangyz <wangyz@bbtree.com>
2020-12-30 16:47:59 +0800

    project init

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+// babel-preset-taro 更多选项和默认值：
+// https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
+module.exports = {
+  presets: [
+    ['taro', {
+      framework: 'react',
+      ts: true
+    }]
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@tarojs/components': '@tarojs/components-rn',
+          '@tarojs/taro': '@tarojs/taro-rn',
+        },
+      },
+    ]
+  ]
+}
